656d3af51f6c65bfb5505d888dd22e440a9cdefb
Ayodeji O <97124713+aosasona@users.noreply.github.com>
2022-12-18 03:03:54 +0000

    Added visual page indicator

--- screens/ReadDocument.tsx
+++ screens/ReadDocument.tsx
@@ -1,9 +1,9 @@
-import { View, Text, ScrollView } from "native-base";
+import { View, Text, ScrollView, Box, useColorMode } from "native-base";
 import { useEffect, useState } from "react";
 import { CombinedFileResultType } from "../types/database";
 import { ScreenProps } from "../types/general";
 import Pdf from "react-native-pdf";
-import { Alert, useWindowDimensions } from "react-native";
+import { Alert, Dimensions, useWindowDimensions } from "react-native";
 import { saveCurrentPage, updateTotalPagesOnLoad } from "../utils/file.util";
 
 export default function ReadDocument({ route, navigation }: ScreenProps) {
@@ -15,7 +15,13 @@ export default function ReadDocument({ route, navigation }: ScreenProps) {
 	}
 
 	const [data, setData] = useState<CombinedFileResultType>(initialData);
-	const { width, height } = useWindowDimensions();
+	const [pageData, setPageData] = useState<{ current: number, total: number }>({
+		current: 1,
+		total: 1
+	})
+
+
+	const { width, height } = Dimensions.get("window")
 
 	useEffect(() => {
 		navigation.setOptions({ title: data?.name || "Read Document" });
@@ -26,20 +32,49 @@ export default function ReadDocument({ route, navigation }: ScreenProps) {
 		navigation.goBack()
 	}
 
+	const onLoadComplete = async (totalNumberOfPages: number,
+		path: string,
+		dimension: { width: number, height: number },
+		tableContents: any) => {
+		await updateTotalPagesOnLoad(data?.id, totalNumberOfPages)
+		setPageData(prev => ({ ...prev, total: totalNumberOfPages }))
+	}
+
+	const onPageChanged = async (page: number, totalNumberOfPages: number) => {
+		await saveCurrentPage(data?.id, page)
+		setPageData(prev => ({ ...prev, current: page }))
+	}
+
 	const source = { uri: data.path }
+	const { colorMode } = useColorMode()
+	const isDark = colorMode === "dark"
 
 	return (
-		<ScrollView px={0}>
+		<View height={height} flex={1} justifyContent="flex-start" alignItems="center" _dark={{ bg: "brand-dark" }} px={0}>
 			<Pdf
 				source={source}
-				onLoadComplete={(numberOfPages, _) => updateTotalPagesOnLoad(data.id, numberOfPages)}
-				onPageChanged={(pageNumber, _) => saveCurrentPage(data?.id, pageNumber)}
+				page={data?.current_page || 1}
+				enablePaging={true}
+				horizontal={true}
+				enableAntialiasing={true}
+				enableAnnotationRendering={true}
+				onLoadComplete={onLoadComplete}
+				onPageChanged={onPageChanged}
 				onError={onError}
-				onPressLink={(uri) => {
-					console.log(`Link pressed: ${uri}`);
-				}}
-				style={{ flex: 1, width, height }}
+				style={{ flex: 1, width, height, backgroundColor: isDark ? "#000000" : "#F5F5F5" }}
 			/>
-		</ScrollView>
+			<Box
+				position="absolute"
+				top={2}
+				right={2}
+				_dark={{ bg: "muted.900" }}
+				_light={{ bg: "muted.100" }}
+				shadow={2}
+				rounded={6}
+				px={2}
+				py={1}>
+				<Text opacity={0.5}>{pageData.current} of {pageData.total}</Text>
+			</Box>
+		</View>
 	)
 }
